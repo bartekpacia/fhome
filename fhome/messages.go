2a1d8fc11cb2fadce6e1ca5ab47c39d65736c03e
Bartek Pacia <barpac02@gmail.com>
2022-04-21 17:26:36 +0200

    add value definitions

--- fhome/messages.go
+++ fhome/messages.go
@@ -1,5 +1,7 @@
 package fhome
 
+import "strconv"
+
 const (
 	ActionOpenClientSession          = "open_client_session"
 	ActionGetMyData                  = "get_my_data"
@@ -8,6 +10,16 @@ const (
 	ActionTouches                    = "touches"
 	ActionGetUserConfig              = "get_user_config"
 	ActionXEvent                     = "xevent"
+)
+
+var (
+	ValueToggle = strconv.FormatInt(0x4001, 8)
+	Value0      = strconv.FormatInt(0x6000, 8)
+	Value20     = strconv.FormatInt(0x6014, 8)
+	Value40     = strconv.FormatInt(0x6028, 8)
+	Value60     = strconv.FormatInt(0x603C, 8)
+	Value80     = strconv.FormatInt(0x6050, 8)
+	Value100    = strconv.FormatInt(0x6064, 8)
 )
 
 type OpenClientSession struct {
